3e2247810503402b267877ff1ac6c22e6d112cdd
Tom Scharstein <trscharstein@gmail.com>
2019-11-05 11:47:11 -0700

    Refactoring, clean unused code, uninstall unused deps

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,22 @@
 #!/usr/bin/env node
 
 const bombino = require("../lib/bombino");
-const inquirer = require("inquirer");
 
-function init() {
-  bombino.greet().then(() => {
-    bombino.prompt().then(answers => {
-      bombino.write(answers).then(name => {
-        bombino.end(name);
-        return null;
-      });
-    });
-  });
+async function init() {
+  let greeting = await bombino.greet();
+  let answers = await bombino.prompt();
+  let name = await bombino.write(answers);
+  return await bombino.end();
 }
-
 init();
+
+// function init() {
+//   bombino.greet().then(() => {
+//     bombino.prompt().then(answers => {
+//       bombino.write(answers).then(name => {
+//         bombino.end(name);
+//         return null;
+//       });
+//     });
+//   });
+// }
