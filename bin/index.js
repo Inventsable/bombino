d29d75161bb86d5714fa29ca682faa7735b9000d
Tom Scharstein <trscharstein@gmail.com>
2019-11-05 17:51:01 -0700

    Scaffolding for Create new Template

--- bin/index.js
+++ bin/index.js
@@ -3,20 +3,12 @@
 const bombino = require("../lib/bombino");
 
 async function init() {
-  let greeting = await bombino.greet();
-  let answers = await bombino.prompt();
-  let name = await bombino.write(answers);
+  let action = await bombino.greet();
+  let answers = await bombino.prompt(action);
+  // Should determine if write new panel or template here
+  let result = answers.isTemplate
+    ? await bombino.createNewTemplate(answers)
+    : await bombino.createPanelFromTemplate(answers);
   return await bombino.end();
 }
 init();
-
-// function init() {
-//   bombino.greet().then(() => {
-//     bombino.prompt().then(answers => {
-//       bombino.write(answers).then(name => {
-//         bombino.end(name);
-//         return null;
-//       });
-//     });
-//   });
-// }
