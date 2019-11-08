c6630eb877f142ff4dae417f51ff55813016b27b
Tom Scharstein <trscharstein@gmail.com>
2019-11-07 18:26:07 -0700

    Refactoring dataflow, adding dynamic config structure

--- bin/index.js
+++ bin/index.js
@@ -3,15 +3,24 @@ require("dotenv").config();
 
 const bombino = require("../lib/bombino");
 
+// REFACTOR THIS TO A BETTER USERFLOW
+// This should branch more organically and intuitively
 async function init() {
   let action = await bombino.landing();
-  let answers = await bombino.prompt(action);
-  // Should determine if write new panel or template here
-  let result = answers.isTemplate
-    ? await bombino.createNewTemplate(answers)
-    : (await !answers.isLocal)
-    ? bombino.createPanelFromGitTemplate(answers)
-    : bombino.createPanelFromLocalTemplate(answers);
+
+  if (/panel|template/.test(action)) {
+    let answers = await bombino.promptCreateTemplateOrPanel(action);
+    let result = answers.isTemplate
+      ? await bombino.createNewTemplate(answers)
+      : (await !answers.isLocal)
+      ? bombino.createPanelFromGitTemplate(answers)
+      : bombino.createPanelFromLocalTemplate(answers);
+  } else if (/utils/.test(action)) {
+    console.log("Open utils");
+  } else if (/config/.test(action)) {
+    console.log("Open config editor");
+  }
+
   // return await bombino.end();
 }
 init();
